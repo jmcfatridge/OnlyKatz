26cc7d6049fd285cc2999788a49f32cdfd2213d7
joseph <josephdanielmcfatridge@gmail.com>
2021-09-02 12:43:45 -0400

    Added the files for material components, added fragments to the view folder which act as view delegates maybe, set the viewBinding and bound it in HomeActivity

--- app/src/main/java/com/project/onlykatz/view/HomeActivity.kt
+++ app/src/main/java/com/project/onlykatz/view/HomeActivity.kt
@@ -3,11 +3,14 @@ package com.project.onlykatz.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.project.onlykatz.R
+import com.project.onlykatz.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
