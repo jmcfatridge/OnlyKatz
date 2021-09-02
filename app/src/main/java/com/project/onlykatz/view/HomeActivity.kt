939e03e92f46c988845b8519962db6021f547661
joseph <josephdanielmcfatridge@gmail.com>
2021-09-02 11:54:40 -0400

    Initial commit

---
+++ app/src/main/java/com/project/onlykatz/view/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.project.onlykatz.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.onlykatz.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/project/onlykatz/viewModel/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.project.onlykatz.viewModel
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+}
