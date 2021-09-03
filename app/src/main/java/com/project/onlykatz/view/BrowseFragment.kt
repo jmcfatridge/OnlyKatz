ef2ba94907ec077cc643d6cd9444bdd3f547f851
joseph <josephdanielmcfatridge@gmail.com>
2021-09-03 11:17:42 -0400

    Set up the nav_graph. Set the home frag to BrowseFragment and added actions that navigate to the DetailsFrag and SettingsFrag. In the BrowseFragment, created a method named initView that "extended" using with. created am onClickListener for the buttons. Then created the findNavController().navigate()

--- app/src/main/java/com/project/onlykatz/view/BrowseFragment.kt
+++ app/src/main/java/com/project/onlykatz/view/BrowseFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.project.onlykatz.R
 import com.project.onlykatz.databinding.FragmentBrowseBinding
 import com.project.onlykatz.databinding.FragmentSettingsBinding
@@ -16,6 +19,22 @@ class BrowseFragment : Fragment(R.layout.fragment_browse) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentBrowseBinding.bind(view)
+        initViews()
+    }
+
+    //with(receiver) is 1 of 5 scope functions
+    private fun initViews() = with(binding) {
+        detailsButton.setOnClickListener {
+            //Without safeArgs
+//            findNavController().navigate(R.id.action_browseFragment_to_detailsFragment)
+
+            //With safeArgs
+            val direction = BrowseFragmentDirections.actionBrowseFragmentToDetailsFragment()
+            findNavController().navigate(direction)
+        }
+        settingsButton.setOnClickListener {
+            findNavController().navigate(R.id.action_browseFragment_to_settingsFragment)
+        }
     }
 
 }
--- app/src/main/java/com/project/onlykatz/view/HomeActivity.kt
+++ app/src/main/java/com/project/onlykatz/view/HomeActivity.kt
@@ -1,7 +1,14 @@
 package com.project.onlykatz.view
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.internal.NavigationSubMenu
 import com.project.onlykatz.R
 import com.project.onlykatz.databinding.ActivityMainBinding
 
@@ -9,8 +16,12 @@ class HomeActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
     }
+
+
+
 }
